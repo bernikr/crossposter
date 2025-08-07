cd833a714be13230bad74055a0fa60741ad4a867
bernikr <git@berni.kr>
2025-08-07 13:14:24 +0200

    working script

---
+++ README.md
@@ -0,0 +1,28 @@
+# Crossposter
+
+A simple Python script that reblogs/retoots posts on mastodon and bluesky.
+
+## Why?
+
+I'm not completely happy with the way [bridging](https://fed.brid.gy/) works between mastodon and bluesky. So I still have accounts on both platforms. I bridge both of them to the other and use this script to automatically reblog/retoot posts from myself on the other platform.
+
+That way people just have to follow one of my accounts to see most of my activity on both platforms.
+
+
+## Setup
+
+`compose.yaml`
+
+```yaml
+services:
+  availability-calendar:
+    image: ghcr.io/bernikr/crossposter:0.1.0
+    environment:
+        BSKY_USERNAME: <username to login to bsky>
+        BSKY_PASSWORD: <(app)password to login to bsky>
+        BSKY_SOURCE_ACCOUNTS: <comma-separated list of accounts to search for posts on bsky>
+        MASTODON_SERVER: <mastodon server to login to>
+        MASTODON_ACCESS_TOKEN: <access token to login to mastodon>
+        MASTODON_SOURCE_ACCOUNTS: <comma-separated list of accounts to search for posts on mastodon>
+        SLEEP_TIME: <time to sleep between searches> (default: 60)
+```
--- main.py
+++ main.py
@@ -1,22 +1,58 @@
-import asyncio
+import logging
 import os
+import time
 
-from atproto import AsyncClient
+from atproto import Client
+from dotenv import load_dotenv
+from mastodon import Mastodon
+
+load_dotenv()
 
 BSKY_USERNAME = os.getenv("BSKY_USERNAME", "")
 BSKY_PASSWORD = os.getenv("BSKY_PASSWORD", "")
 BSKY_SOURCE_ACCOUNTS = os.getenv("BSKY_SOURCE_ACCOUNTS", "").split(",")
 
+MASTODON_SERVER = os.getenv("MASTODON_SERVER", "")
+MASTODON_ACCESS_TOKEN = os.getenv("MASTODON_ACCESS_TOKEN", "")
+MASTODON_SOURCE_ACCOUNTS = os.getenv("MASTODON_SOURCE_ACCOUNTS", "").split(",")
 
-async def main() -> None:
-    bsky = AsyncClient()
-    await bsky.login(BSKY_USERNAME, BSKY_PASSWORD)
+SLEEP_TIME = int(os.getenv("SLEEP_TIME", "60"))
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def bsky_repost() -> None:
+    bsky = Client()
+    bsky.login(BSKY_USERNAME, BSKY_PASSWORD)
     for source_account in BSKY_SOURCE_ACCOUNTS:
-        feed = await bsky.get_author_feed(source_account)
+        feed = bsky.get_author_feed(source_account)
         for post in feed.feed:
-            if post.post.viewer and not post.post.viewer.repost:
-                await bsky.repost(post.post.uri, post.post.cid)
+            if post.post.viewer and post.post.viewer.repost:
+                continue
+            bsky.repost(post.post.uri, post.post.cid)
+            logger.info("Reposted bsky post %s", post.post.cid)
+
+
+def mastodon_repost() -> None:
+    mastodon = Mastodon(access_token=MASTODON_ACCESS_TOKEN, api_base_url=MASTODON_SERVER)
+    me = mastodon.me()
+    for source_account in MASTODON_SOURCE_ACCOUNTS:
+        account = mastodon.account_lookup(source_account)
+        feed = mastodon.account_statuses(account)
+        for post in feed:
+            if post.reblog and post.reblog.account.id == me.id:
+                continue
+            if post.reblogged:
+                continue
+            mastodon.status_reblog(post.id)
+            logger.info("Reposted mastodon post %s", post.id)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    while True:
+        logger.info("Searching for new posts...")
+        bsky_repost()
+        mastodon_repost()
+        logger.info("Sleeping for %i seconds...", SLEEP_TIME)
+        time.sleep(SLEEP_TIME)
